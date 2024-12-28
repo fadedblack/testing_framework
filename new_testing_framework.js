1ec7fc0136b11da9f67c3f6202a6543e682cac87
Inkeet C <inkeetcha234@gmail.com>
2024-12-28 16:11:10 +0530

    add logic to handle objects

--- new_testing_framework.js
+++ new_testing_framework.js
@@ -7,6 +7,13 @@ const areEqual = function (value1, value2) {
     return value1.every((element, index) => areEqual(element, value2[index]));
   }
 
+  if (typeof value1 === "object") {
+    return areEqual(
+      Object.entries(value1).sort(),
+      Object.entries(value2).sort()
+    );
+  }
+
   return value1 === value2;
 };
 
@@ -24,6 +31,7 @@ const test2 = function (testCases) {
   console.table(failed);
 };
 
+// test for numbers and arrays
 const testCases = [
   // test for two equal arrays of same lengths
   [
@@ -140,4 +148,82 @@ const testCases = [
   ],
 ];
 
+// test for objects
+const objectTestCases = [
+  // test for unequal objects
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: 20 },
+      { name: "faded", age: 5 },
+    ],
+    false,
+  ],
+  // test for equal objects
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: 20 },
+      { name: "inkeet", age: 20 },
+    ],
+    true,
+  ],
+  // test for two unequal array attribute
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: [20] },
+      { name: "inkeet", age: [19] },
+    ],
+    false,
+  ],
+  // test for two equal array attribute
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: [20] },
+      { name: "inkeet", age: [20] },
+    ],
+    true,
+  ],
+  // test for two unequal object attribute
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: { indian: 20, western: 30 } },
+      { name: "inkeet", age: { indian: 34, western: 59 } },
+    ],
+    false,
+  ],
+  // test for two equal object attributes
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: { indian: 20, western: 30 } },
+      { name: "inkeet", age: { indian: 20, western: 30 } },
+    ],
+    true,
+  ],
+  // test for two unequal object and array attributes
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: { indian: 20, western: 30 }, hobby: ["reading"] },
+      { name: "inkeet", age: { indian: 20, western: 30 }, hobby: ["sleeping"] },
+    ],
+    false,
+  ],
+  // test for two equal object and array attributes
+  [
+    areEqual,
+    [
+      { name: "inkeet", age: { indian: 20, western: 30 }, hobby: ["reading"] },
+      { name: "inkeet", age: { indian: 20, western: 30 }, hobby: ["reading"] },
+    ],
+    true,
+  ],
+];
+
 test2(testCases);
+
+test2(objectTestCases);
